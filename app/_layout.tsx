b403c0a094b4d323ef77f192eafefbfb2ebbc091
narenkumar6695 <narenkumar6695@gmail.com>
2025-06-25 15:54:02 +0530

    deleted HomeScreen into index file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,3 @@
-import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
@@ -6,15 +5,6 @@ import { View } from "react-native";
 import { Provider } from "react-redux";
 import { store } from "../src/store";
 import "./global.css";
-
-const customTheme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    background: "#f3f4f6",
-    text: "#1f2937",
-  },
-};
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
@@ -30,22 +20,20 @@ export default function RootLayout() {
 
   return (
     <Provider store={store}>
-      <ThemeProvider value={customTheme}>
-        <View className="flex-1 font-sans">
-          <Stack
-            screenOptions={{
-              headerStyle: {
-                backgroundColor: "#f3f4f6",
-              },
-              headerTintColor: "#1f2937",
-            }}
-          >
-            <Stack.Screen name="index" options={{ headerShown: false }} />
-            <Stack.Screen name="+not-found" />
-          </Stack>
-          <StatusBar style="auto" />
-        </View>
-      </ThemeProvider>
+      <View className="flex-1 font-sans">
+        <Stack
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: "#f3f4f6",
+            },
+            headerTintColor: "#1f2937",
+          }}
+        >
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+        <StatusBar style="auto" />
+      </View>
     </Provider>
   );
 }
